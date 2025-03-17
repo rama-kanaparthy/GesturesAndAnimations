a056a5a81fb185ab70d9f93d011b1006c38fb467
Rama Brahamam Kanparthy <ramabrahamam1991@gmail.com>
2025-03-17 21:26:32 +0530

    compose

--- app/src/main/java/com/rama/gesturesanimations/MainActivity.kt
+++ app/src/main/java/com/rama/gesturesanimations/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.rama.gesturesanimations.ui.theme.GesturesAnimationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +31,7 @@ class MainActivity : ComponentActivity() {
             GesturesAnimationsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Gestures",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,16 +42,46 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var counter = remember { mutableStateOf(0) }
+    Column(
+        modifier = Modifier.padding(16.dp).fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Box(
+            contentAlignment = Alignment.TopCenter,
+            modifier = Modifier.padding(16.dp).fillMaxWidth().weight(1f)
+        ) {
+            Text(
+                text = "${counter.value}",
+                modifier = modifier,
+                fontSize = 100.sp
+            )
+        }
+
+        Box(
+            contentAlignment = Alignment.BottomCenter,
+            modifier = Modifier.padding(16.dp).fillMaxWidth().weight(1f)
+        ) {
+            Button(
+                modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                onClick = {
+                    counter.value++
+                }
+            ) {
+                Text(
+                    text = name,
+                    fontSize = 24.sp
+                )
+            }
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = "id:pixel_8a")
 @Composable
 fun GreetingPreview() {
     GesturesAnimationsTheme {
-        Greeting("Android")
+        Greeting("Increment")
     }
 }
