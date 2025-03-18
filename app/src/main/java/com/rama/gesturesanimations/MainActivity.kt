13328809546f2b6e518b1275a0bbcdda1bc40818
Rama Brahamam Kanparthy <ramabrahamam1991@gmail.com>
2025-03-19 00:02:56 +0530

    compose dragging

--- app/src/main/java/com/rama/gesturesanimations/MainActivity.kt
+++ app/src/main/java/com/rama/gesturesanimations/MainActivity.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.rama.gesturesanimations.compose.DragWithAnimationExample
 import com.rama.gesturesanimations.compose.VerticalDragExample
 import com.rama.gesturesanimations.ui.theme.GesturesAnimationsTheme
 
